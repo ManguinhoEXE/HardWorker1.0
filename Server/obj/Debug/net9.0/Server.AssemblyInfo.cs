d1c60bf4205201fed9a617ff875070c008bfea35
ManguinhoEXE <jpaul1706@hotmail.com>
2025-09-08 17:13:53 -0500

    HardWorker 1.0 Done

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea739995ad11541bcad8bc832076ca1140c14db1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecf8e873112580e8d083bc400f7c6fde3207a47d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
